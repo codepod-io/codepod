e140181e8cee30b7000aeb0e1750cb0d73100f66
Hebi Li <hebi@lihebi.com>
2021-02-01 18:51:37 +0800

    clear up front-end

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import 'codemirror.css'
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import './index.css';
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,19 +10,5 @@ ReactDOM.render(
     {/* <div id="raweditor"></div>
     <textarea id="editor" className="h-auto"></textarea> */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
-// var myCodeMirror = CodeMirror(document.body);
-
-// var myCodeMirror = CodeMirror(document.getElementById("raweditor"), {
-//   value: "function myScript(){return 100;}\n",
-//   mode:  "javascript"
-// });
