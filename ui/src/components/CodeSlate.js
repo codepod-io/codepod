b4b791c7f97126b8e8b3dbd9ecbdfd2753d97fb7
Hebi Li <lihebi.com@gmail.com>
2021-07-16 17:49:19 -0500

    add julia prisma

--- ui/src/components/CodeSlate.js
+++ ui/src/components/CodeSlate.js
@@ -3,6 +3,8 @@ import "prismjs/components/prism-python";
 import "prismjs/components/prism-php";
 import "prismjs/components/prism-sql";
 import "prismjs/components/prism-java";
+import "prismjs/components/prism-julia";
+
 import React, { useState, useCallback, useMemo } from "react";
 import { Slate, Editable, withReact } from "slate-react";
 import { Text, createEditor } from "slate";
