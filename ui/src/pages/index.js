3c1cf7d58947c114928f4dd914cd5addf5b5dc6e
Hebi Li <lihebi.com@gmail.com>
2021-07-21 18:34:20 -0500

    add lipsum to index

--- ui/src/pages/index.js
+++ ui/src/pages/index.js
@@ -2,11 +2,72 @@ import { Box, Text, Heading, Center } from "@chakra-ui/layout";
 
 export default function Home() {
   return (
-    <Center>
-      <Box>
+    <Box maxW="lg" m="auto">
+      <Center>
         <Heading>CodePod</Heading>
         <Text>Your next IDE is not an IDE</Text>
+      </Center>
+      <Box>
+        <Heading>Sidebar</Heading>
+        <Box>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          luctus fringilla urna accumsan sollicitudin. Proin tellus eros,
+          imperdiet vitae justo ut, blandit vulputate sapien. Proin feugiat
+          dolor sed ligula interdum mollis. Curabitur laoreet elementum
+          efficitur. Proin id laoreet lacus, sit amet mattis libero. Mauris eget
+          ultrices enim, ut bibendum dolor. Ut eu lorem mattis, imperdiet turpis
+          a, porta ante. Maecenas rutrum, urna eu sollicitudin sagittis, dui
+          lacus porta augue, sed faucibus ipsum mauris sed metus. In consequat
+          odio ac bibendum scelerisque. Vivamus non tortor sagittis ligula
+          ullamcorper aliquam ut quis arcu. Fusce nec lorem ac tellus malesuada
+          laoreet. Suspendisse sit amet tellus vel dolor malesuada semper in nec
+          justo.
+        </Box>
+        <Box>
+          Suspendisse id elit sodales, efficitur dolor id, dapibus dui.
+          Curabitur sed metus orci. Nunc bibendum sapien sed auctor posuere.
+          Duis ut nisl scelerisque, blandit est condimentum, laoreet massa.
+          Aliquam tincidunt fermentum nunc id tempor. Phasellus laoreet lacus
+          vel ipsum malesuada blandit. Proin massa mi, imperdiet sit amet urna
+          ut, eleifend condimentum dolor. Sed at urna augue. Suspendisse
+          potenti. Donec quis erat et leo vehicula laoreet ac id libero.
+        </Box>
+        Proin elementum venenatis tortor, et euismod mi aliquet in. Curabitur
+        metus nibh, iaculis a dui nec, sollicitudin molestie neque. Suspendisse
+        ornare quis justo ornare viverra. Ut sagittis et urna fermentum
+        scelerisque. Pellentesque sit amet lacus purus. Sed bibendum, odio ac
+        interdum tristique, augue leo dictum ex, quis pellentesque sapien lectus
+        id massa. Sed eget justo a lacus condimentum vehicula. Curabitur lorem
+        massa, ultrices nec suscipit eget, porttitor sit amet arcu. Morbi sit
+        amet ligula vitae mi placerat iaculis. Class aptent taciti sociosqu ad
+        litora torquent per conubia nostra, per inceptos himenaeos.
+        <Box>
+          Pellentesque ac magna non dolor venenatis mattis volutpat id ex. Fusce
+          sagittis ultricies erat, sed imperdiet metus suscipit ut. Praesent
+          tincidunt iaculis libero, sit amet feugiat elit pellentesque sed.
+          Pellentesque interdum, ante nec tincidunt luctus, elit quam semper
+          libero, ac venenatis tellus purus quis elit. Nullam gravida a mauris
+          non malesuada. Curabitur interdum erat nulla, sit amet aliquam purus
+          luctus sed. Donec eget placerat tortor, et semper orci. Donec nec
+          blandit arcu. Vivamus volutpat libero sit amet malesuada pulvinar.
+          Nunc id mollis erat, eu hendrerit orci. Vivamus imperdiet odio vitae
+          semper lobortis. In vehicula mauris vitae urna aliquet, et pharetra
+          eros pretium. Nulla interdum purus nec semper vulputate. Curabitur
+          venenatis nisi mauris, ac fermentum dui iaculis a.
+        </Box>
+        <Box>
+          Interdum et malesuada fames ac ante ipsum primis in faucibus.
+          Suspendisse eu eros vitae leo egestas tristique. Etiam at quam
+          condimentum, blandit enim ac, tincidunt mauris. Donec dignissim
+          malesuada mi, eget sollicitudin velit fermentum nec. Sed vel ex
+          efficitur, congue enim nec, finibus odio. Sed eros leo, bibendum id
+          lectus at, mollis viverra nunc. Integer et velit mattis, lacinia lacus
+          placerat, porta eros. Fusce id justo sit amet nulla egestas malesuada.
+          Aliquam suscipit maximus purus, id fermentum nibh iaculis sed. Sed
+          elementum vehicula quam sit amet faucibus. Maecenas et tempus elit.
+          Mauris ac velit urna.
+        </Box>
       </Box>
-    </Center>
+    </Box>
   );
 }
