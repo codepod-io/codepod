e03c14049b4139d7ce4fd9f65eea19e8d8c45571
Hebi Li <lihebi.com@gmail.com>
2021-11-08 19:27:48 -0600

    import ui App.js

---
+++ app/config-overrides.js
@@ -0,0 +1,11 @@
+const ModuleScopePlugin = require("react-dev-utils/ModuleScopePlugin");
+
+module.exports = function override(config, env) {
+  config.resolve.plugins = config.resolve.plugins.filter(
+    (plugin) => !(plugin instanceof ModuleScopePlugin)
+  );
+
+  config.resolve.symlinks = false;
+
+  return config;
+};
