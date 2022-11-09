44ac932200b8b7b07e310a522b243ca340679195
Hebi Li <lihebi.com@gmail.com>
2022-11-09 15:38:54 -0800

    bump version to 0.4.4 due to miscreation of 0.4.3

--- ui/src/pages/about.tsx
+++ ui/src/pages/about.tsx
@@ -7,7 +7,7 @@ export default function About() {
         textAlign: "center",
       }}
     >
-      <Box>CodePod version 0.4.3</Box>
+      <Box>CodePod version 0.4.4</Box>
     </Box>
   );
 }
--- ui/src/pages/profile.tsx
+++ ui/src/pages/profile.tsx
@@ -34,7 +34,7 @@ export default function Profile() {
                 Name {me.firstname} {me.lastname}
               </Box>
               <Box> Email: {me.email}</Box>
-              <Box>CodePod version 0.4.3</Box>
+              <Box>CodePod version 0.4.4</Box>
             </Stack>
           </Paper>
           <Divider />
