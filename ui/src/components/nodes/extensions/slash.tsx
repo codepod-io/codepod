76ffecfa4a10e3b5472d16d18ff6babdf4c58852
Hebi Li <lihebi.com@gmail.com>
2023-07-27 17:53:28 -0700

    clean up typing info

--- ui/src/components/nodes/extensions/slash.tsx
+++ ui/src/components/nodes/extensions/slash.tsx
@@ -43,110 +43,12 @@ import {
   Suggester,
 } from "@remirror/pm/suggest";
 import { PlainExtension } from "remirror";
+import {
+  MentionChangeHandlerCommandAttributes,
+  MentionOptions,
+  NamedMentionExtensionAttributes,
+} from "remirror/extensions";
 
-/**
- * The static settings passed into a mention
- */
-export interface MentionOptions
-  extends Pick<
-    Suggester,
-    | "invalidNodes"
-    | "validNodes"
-    | "invalidMarks"
-    | "validMarks"
-    | "isValidPosition"
-    | "disableDecorations"
-  > {
-  /**
-   * Provide a custom tag for the mention
-   */
-  mentionTag?: Static<string>;
-
-  /**
-   * Provide the custom matchers that will be used to match mention text in the
-   * editor.
-   */
-  matchers: Static<MentionExtensionMatcher[]>;
-
-  /**
-   * Text to append after the mention has been added.
-   *
-   * **NOTE**: If you're using whitespace characters but it doesn't seem to work
-   * for you make sure you're using the css provided in `@remirror/styles`.
-   *
-   * The `white-space: pre-wrap;` is what allows editors to add space characters
-   * at the end of a section.
-   *
-   * @defaultValue ''
-   */
-  appendText?: string;
-
-  /**
-   * Tag for the prosemirror decoration which wraps an active match.
-   *
-   * @defaultValue 'span'
-   */
-  suggestTag?: string;
-
-  /**
-   * Called whenever a suggestion becomes active or changes in any way.
-   *
-   * @remarks
-   *
-   * It receives a parameters object with the `reason` for the change for more
-   * granular control.
-   *
-   * The second parameter is a function that can be called to handle exits
-   * automatically. This is useful if you're mention can be any possible value,
-   * e.g. a `#hashtag`. Call it with the optional attributes to automatically
-   * create a mention.
-   *
-   * @defaultValue () => void
-   */
-  onChange?: Handler<MentionChangeHandler>;
-
-  /**
-   * Listen for click events to the mention extension.
-   */
-  onClick?: Handler<
-    (event: MouseEvent, markRange: GetMarkRange) => boolean | undefined | void
-  >;
-
-  /**
-   * A predicate check for whether the mention is valid. It proves the mention
-   * mark and it's attributes as well as the text it contains.
-   *
-   * This is used for checking that a recent update to the document hasn't made
-   * a mention invalid.
-   *
-   * For example a mention for `@valid` => `valid` would be considered
-   * invalidating. Return false to remove the mention.
-   *
-   * @param attrs - the attrs for the mention
-   * @param text - the text which is wrapped by the mention
-   */
-  isMentionValid?: (
-    attrs: NamedMentionExtensionAttributes,
-    text: string
-  ) => boolean;
-}
-
-/**
- * The mention extension wraps mentions as a prosemirror mark. It allows for
- * fluid social experiences to be built. The implementation was inspired by the
- * way twitter and similar social sites allows mentions to be edited after
- * they've been created.
- *
- * @remarks
- *
- * Mentions have the following features
- * - An activation character or regex pattern which you define.
- * - A min number of characters before mentions are suggested
- * - Ability to exclude matching character.
- * - Ability to wrap content in a decoration which excludes mentions from being
- *   suggested.
- * - Decorations for in-progress mentions
- */
 @extension<MentionOptions>({
   defaultOptions: {
     mentionTag: "a" as const,
@@ -260,9 +162,7 @@ export class SlashExtension extends PlainExtension<MentionOptions> {
   }
 
   @command()
-  cancelMention(
-    config: NamedMentionExtensionAttributes & KeepSelectionProps
-  ): CommandFunction {
+  cancelMention(config: NamedMentionExtensionAttributes): CommandFunction {
     const {
       range,
       appendText,
@@ -284,144 +184,6 @@ export class SlashExtension extends PlainExtension<MentionOptions> {
   }
 }
 
-export interface OptionalMentionExtensionProps {
-  /**
-   * The text to append to the replacement.
-   *
-   * @defaultValue ''
-   */
-  appendText?: string;
-
-  /**
-   * The range of the requested selection.
-   */
-  range?: RangeWithCursor;
-
-  /**
-   * Whether to replace the whole match (`full`) or just the part up until the
-   * cursor (`partial`).
-   */
-  replacementType?: keyof MatchValue;
-}
-
-interface KeepSelectionProps {
-  /**
-   * Whether to preserve the original selection after the replacement has
-   * occurred.
-   */
-  keepSelection?: boolean;
-}
-
-/**
- * The attrs that will be added to the node. ID and label are plucked and used
- * while attributes like href and role can be assigned as desired.
- */
-export type MentionExtensionAttributes = ProsemirrorAttributes<
-  OptionalMentionExtensionProps & {
-    /**
-     * A unique identifier for the suggesters node
-     */
-    id: string;
-
-    /**
-     * The text to be placed within the suggesters node
-     */
-    label: string;
-  }
->;
-
-export type NamedMentionExtensionAttributes = ProsemirrorAttributes<
-  OptionalMentionExtensionProps & {
-    /**
-     * A unique identifier for the suggesters node
-     */
-    id: string;
-
-    /**
-     * The text to be placed within the suggesters node
-     */
-    label: string;
-  } & {
-    /**
-     * The identifying name for the active matcher. This is stored as an
-     * attribute on the HTML that will be produced
-     */
-    name: string;
-  }
->;
-
-/**
- * The options for the matchers which can be created by this extension.
- */
-export interface MentionExtensionMatcher
-  extends Pick<
-    Suggester,
-    | "char"
-    | "name"
-    | "startOfLine"
-    | "supportedCharacters"
-    | "validPrefixCharacters"
-    | "invalidPrefixCharacters"
-    | "matchOffset"
-    | "suggestClassName"
-  > {
-  /**
-   * Provide customs class names for the completed mention
-   */
-  mentionClassName?: string;
-
-  /**
-   * Text to append after the suggestion has been added.
-   *
-   * @defaultValue ''
-   */
-  appendText?: string;
-}
-
-export type MentionChangeHandlerCommand = (
-  attrs?: MentionChangeHandlerCommandAttributes
-) => void;
-
-export interface MentionChangeHandlerProps extends SuggestChangeHandlerProps {
-  /**
-   * The default text to be appended if text should be appended.
-   */
-  defaultAppendTextValue: string;
-}
-
-/**
- * A handler that will be called whenever the the active matchers are updated or
- * exited. The second argument which is the exit command is a function which is
- * only available when the matching suggester has been exited.
- */
-export type MentionChangeHandler = (
-  handlerState: MentionChangeHandlerProps,
-  command: (attrs?: MentionChangeHandlerCommandAttributes) => void
-) => void;
-
-/**
- * The dynamic properties used to change the behavior of the mentions created.
- */
-export type MentionChangeHandlerCommandAttributes = ProsemirrorAttributes<
-  Partial<
-    Pick<MentionExtensionAttributes, "appendText" | "replacementType">
-  > & {
-    /**
-     * The ID to apply the mention.
-     *
-     * @defaultValue query.full
-     */
-    id?: string;
-
-    /**
-     * The text that is displayed within the mention bounds.
-     *
-     * @defaultValue text.full
-     */
-    label?: string;
-  }
->;
-
 /**
  * The default matcher to use when none is provided in options
  */
@@ -437,55 +199,6 @@ const DEFAULT_MATCHER = {
   matchOffset: 1,
   mentionClassName: "mention",
 };
-
-/**
- * Check that the attributes exist and are valid for the mention update command
- * method.
- */
-function isValidMentionAttributes(
-  attributes: unknown
-): attributes is NamedMentionExtensionAttributes {
-  return !!(
-    attributes &&
-    isPlainObject(attributes) &&
-    attributes.id &&
-    isString(attributes.id) &&
-    attributes.label &&
-    isString(attributes.label) &&
-    attributes.name &&
-    isString(attributes.name)
-  );
-}
-
-/**
- * Gets the matcher from the list of matchers if it exists.
- *
- * @param name - the name of the matcher to find
- * @param matchers - the list of matchers to search through
- */
-function getMatcher(name: string, matchers: MentionExtensionMatcher[]) {
-  const matcher = matchers.find((matcher) => matcher.name === name);
-  return matcher ? { ...DEFAULT_MATCHER, ...matcher } : undefined;
-}
-
-/**
- * Get the append text value which needs to be handled carefully since it can
- * also be an empty string.
- */
-function getAppendText(
-  preferred: string | undefined,
-  fallback: string | undefined
-) {
-  if (isString(preferred)) {
-    return preferred;
-  }
-
-  if (isString(fallback)) {
-    return fallback;
-  }
-
-  return DEFAULT_MATCHER.appendText;
-}
 
 /**
  * Checks whether the mention is valid and hasn't been edited since being
--- ui/src/components/nodes/extensions/useSlash.tsx
+++ ui/src/components/nodes/extensions/useSlash.tsx
@@ -16,52 +16,17 @@ import { useExtensionEvent, useHelpers } from "@remirror/react-core";
 
 import {
   FloatingWrapper,
+  MentionState,
   MenuNavigationOptions,
   useCommands,
+  UseMentionProps,
+  UseMentionReturn,
   useMenuNavigation,
   UseMenuNavigationReturn,
 } from "@remirror/react";
 import { cx, replaceText } from "remirror";
 
-export interface MentionState<
-  Data extends MentionExtensionAttributes = MentionExtensionAttributes
-> extends Pick<SuggestChangeHandlerProps, "name" | "query" | "text" | "range"> {
-  /**
-   * This command when the mention is active.
-   */
-  command: (item: Data) => void;
-
-  /**
-   * The reason for the change.
-   */
-  reason: ChangeReason;
-}
-
-export interface UseMentionReturn<
-  Data extends MentionExtensionAttributes = MentionExtensionAttributes
-> extends UseMenuNavigationReturn<Data> {
-  state: MentionState<Data> | null;
-}
-
-/**
- * A hook that provides the state for social mentions that responds to
- * keybindings and key-presses from the user.
- *
- * This is used by the `SocialMentionDropdown` component and can be used by you
- * for a customized component.
- *
- * The only prop required is the list of data in order to support keybinding and
- * properly selecting the index for you. The data must have a `label` and `id`
- * key. The label is the text that should be shown inside the mention and the
- * `id` is whatever unique identifier that can be used.
- *
- * You can also add other supported attributes which will be added to the
- * mention node, like `href` and whatever you decide.
- *
- * @param list - the list of data from which an index can be calculated. Must
- * include at least an `id` and a `label`.
- */
-export function useMention<
+function useSlash<
   Data extends MentionExtensionAttributes = MentionExtensionAttributes
 >(props: UseMentionProps<Data>): UseMentionReturn<Data> {
   const {
@@ -216,55 +181,10 @@ export function useMention<
   return useMemo(() => ({ ...menu, state }), [menu, state]);
 }
 
-export interface UseMentionProps<
-  Data extends MentionExtensionAttributes = MentionExtensionAttributes
-> extends MenuNavigationOptions {
-  /**
-   * The list of data from which an index can be calculated. Must include at
-   * least an `id` and a `label`.
-   */
-  items: Data[];
-
-  /**
-   * This method is called when a user induced exit happens before a mention has
-   * been created. It receives the state, and gives the consumer of this hook
-   * the opportunity to manually create their own mention
-   *
-   * Leave this undefined to ignore exits.
-   *
-   * To enable automatic exit handling. The following will automatically set the
-   * id to be the query and the label to be the full matching text. Extra attrs
-   * like `href` can be added by you to the attrs object parameter.
-   *
-   * ```ts
-   * const mentionState = useMention({ items, onExit(_, command) => command(), });
-   * ```
-   */
-  onExit?: UseMentionExitHandler<Data>;
-
-  /**
-   * Whether matches should be permanently ignored when the user presses escape.
-   *
-   * @defaultValue true
-   */
-  ignoreMatchesOnDismiss?: boolean;
-}
-
-export type UseMentionExitHandler<
-  Data extends MentionExtensionAttributes = MentionExtensionAttributes
-> = (
-  props: OnExitProps<Data>,
-  command: (attrs?: Partial<Data>) => void
-) => void;
-
-type OnExitProps<
-  Data extends MentionExtensionAttributes = MentionExtensionAttributes
-> = Replace<Omit<MentionState<Data>, "command">, { reason: ExitReason }>;
-
 export function SlashSuggestor(): JSX.Element {
   const [users, setUsers] = useState<MentionExtensionAttributes[]>([]);
   const { state, getMenuProps, getItemProps, indexIsHovered, indexIsSelected } =
-    useMention({
+    useSlash({
       items: users,
     });
 
