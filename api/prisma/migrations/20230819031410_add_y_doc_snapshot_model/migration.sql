d291948b587332965950f2dd70433b8a3cf63baf
Sen Wang <senwang@Sens-MBP-M2Pro.local>
2023-08-18 20:19:27 -0700

    clean prisma migration sql

--- api/prisma/migrations/20230819031410_add_y_doc_snapshot_model/migration.sql
+++ api/prisma/migrations/20230819031410_add_y_doc_snapshot_model/migration.sql
@@ -3,11 +3,11 @@ CREATE TABLE "YDocSnapshot" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "message" TEXT,
-    "yDocBlob" BYTEA,
-    "repoId" TEXT,
+    "yDocBlob" BYTEA NOT NULL,
+    "repoId" TEXT NOT NULL,
 
     CONSTRAINT "YDocSnapshot_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
-ALTER TABLE "YDocSnapshot" ADD CONSTRAINT "YDocSnapshot_repoId_fkey" FOREIGN KEY ("repoId") REFERENCES "Repo"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "YDocSnapshot" ADD CONSTRAINT "YDocSnapshot_repoId_fkey" FOREIGN KEY ("repoId") REFERENCES "Repo"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
