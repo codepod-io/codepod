23737b99cdde320a685a8fb905919b02b96f2edf
Hebi Li <lihebi.com@gmail.com>
2023-08-24 18:47:20 -0700

    fix dev listening

--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { port: 3000 },
   plugins: [react({ tsDecorators: true })],
 });
