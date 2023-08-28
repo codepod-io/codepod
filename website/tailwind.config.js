01b1a612edcf63f8425d07898bf4eab565709c4e
Hebi Li <lihebi.com@gmail.com>
2023-08-28 12:40:48 -0700

    move website to its standalone repo

--- website/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  corePlugins: { preflight: false, container: false },
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
