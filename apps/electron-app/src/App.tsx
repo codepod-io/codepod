e962717852688bda841e0371b547a937d3f7c8a7
Hebi Li <lihebi.com@gmail.com>
2023-08-28 18:41:11 -0700

    feat: add native-rendered Electron app (#491)
    * init electron app
    
    * use pnpm
    
    * format
    
    * add react
    
    * add @codepod/ui: workspace

---
+++ apps/electron-app/src/App.tsx
@@ -0,0 +1,13 @@
+import * as ReactDOM from "react-dom";
+
+import { Test } from "@codepod/ui";
+
+function App() {
+  return <Test />;
+}
+
+function render() {
+  ReactDOM.render(<App />, document.body);
+}
+
+render();
