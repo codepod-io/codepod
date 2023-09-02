9b24a675204ea9bd63b260efe129b63c34cc1fbf
Sen Wang <senwang@Sens-MBP-M2Pro.local>
2023-09-01 21:21:52 -0700

    clean up

--- apps/ui/src/components/Sidebar.tsx
+++ apps/ui/src/components/Sidebar.tsx
@@ -2,8 +2,6 @@ import { useEffect, useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
 import Divider from "@mui/material/Divider";
 import Tooltip from "@mui/material/Tooltip";
 import IconButton from "@mui/material/IconButton";
