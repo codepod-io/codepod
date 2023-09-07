a21bde547032c8f765afc60bc12b6460f3259243
Hebi Li <lihebi.com@gmail.com>
2023-09-06 19:44:50 -0700

    add ui-web

---
+++ apps/ui-web/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import checker from "vite-plugin-checker";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: { port: 3000 },
+  plugins: [
+    react({ tsDecorators: true }),
+    checker({
+      // e.g. use TypeScript check
+      typescript: true,
+    }),
+  ],
+});
